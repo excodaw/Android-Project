459eaa879c4d1184098de3b8d0fb8b128631f0a9
excodaw <dhtpgkr1999@gmail.com>
2022-09-24 16:10:16 +0900

    안드로이드 스튜디오

---
+++ app/src/main/java/com/example/githubproject/RecordFragment.java
@@ -0,0 +1,32 @@
+package com.example.githubproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecordFragment extends Fragment {
+    public static RecordFragment newInstance(String param1, String param2) {
+        RecordFragment fragment = new RecordFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_record, container, false);
+    }
+}
