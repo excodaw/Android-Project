3803c87ce0ec5de20729563f95a1840e4ef7e600
ahnjinwon <anjy0821@naver.com>
2022-10-21 13:29:30 +0900

    ahn

--- app/src/main/java/com/example/githubproject/MainActivity.java
+++ app/src/main/java/com/example/githubproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.githubproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.icu.text.AlphabeticIndex;
@@ -12,8 +13,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,14 +31,18 @@ public class MainActivity extends AppCompatActivity {
         RecordFragment = new RecordFragment();
         SettingFragment = new SettingFragment();
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                findViewById(R.id.button).setVisibility(View.GONE);
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                 Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_SHORT).show();
                 transaction.replace(R.id.container, SettingFragment);
                 transaction.commit();
+
 
             }
         });
@@ -51,17 +54,19 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.first_tab:
-
+                        findViewById(R.id.button).setVisibility(View.VISIBLE);
                         Toast.makeText(getApplicationContext(),"WorkOut",Toast.LENGTH_SHORT).show();
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,WorkOutFragment).commit();
                         return true;
 
                     case R.id.second_tab:
+                        findViewById(R.id.button).setVisibility(View.VISIBLE);
                         Toast.makeText(getApplicationContext(),"Routine",Toast.LENGTH_SHORT).show();
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,RoutineFragment).commit();
                         return true;
 
                     case R.id.third_tab:
+                        findViewById(R.id.button).setVisibility(View.VISIBLE);
                         Toast.makeText(getApplicationContext(),"Record",Toast.LENGTH_SHORT).show();
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,RecordFragment).commit();
                         return true;
@@ -72,3 +77,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
