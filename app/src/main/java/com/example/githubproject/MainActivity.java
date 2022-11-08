1086848cdff87e5e4db350c5f2b847940c4c8f6e
excodaw <dhtpgkr1999@gmail.com>
2022-11-08 14:35:48 +0900

    플로팅 버튼을 통해 운동 추가 액티비티로 전환
    뒤로가기 질의 다이얼로그 생성
    
    세팅 프래그먼트 -> 세팅 액티비티로 변경

--- app/src/main/java/com/example/githubproject/MainActivity.java
+++ app/src/main/java/com/example/githubproject/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.githubproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.icu.text.AlphabeticIndex;
 import android.os.Bundle;
 import android.view.MenuInflater;
@@ -27,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     WorkOutFragment WorkOutFragment;
     RoutineFragment RoutineFragment;
     RecordFragment RecordFragment;
-    SettingFragment SettingFragment;
     BottomNavigationView bottomNavigationView;
 
     public boolean onCreateOptionsMenu(Menu menu)    {
@@ -35,16 +38,13 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.setting_menu:
+                findViewById(R.id.fab).setVisibility(View.GONE);
                 findViewById(R.id.setting_menu).setVisibility(View.GONE);
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_SHORT).show();
-                transaction.replace(R.id.container, SettingFragment);
-                transaction.commit();
-
+                Intent intent = new Intent(this, SettingActivity.class);
+                startActivity(intent);
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -58,8 +58,8 @@ public class MainActivity extends AppCompatActivity {
         WorkOutFragment = new WorkOutFragment();
         RoutineFragment = new RoutineFragment();
         RecordFragment = new RecordFragment();
-        SettingFragment = new SettingFragment();
 
+        findViewById(R.id.fab).setVisibility(View.VISIBLE);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onMenuItemClick(MenuItem menuItem) {
                         switch(menuItem.getItemId()) {
                             case R.id.add_routine:
-                                Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), AddWorkOutActivity.class));
                                 return true;
                             case R.id.add_workout:
                                 Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_SHORT).show();
@@ -88,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
                 popup.show();
             }
         });
-
         getSupportFragmentManager().beginTransaction().replace(R.id.container,WorkOutFragment).commit();
 
         bottomNavigationView = findViewById(R.id.bottom_menu);
@@ -98,16 +97,19 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.first_tab:
                         findViewById(R.id.setting_menu).setVisibility(View.VISIBLE);
+                        findViewById(R.id.fab).setVisibility(View.VISIBLE);
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,WorkOutFragment).commit();
                         return true;
 
                     case R.id.second_tab:
                         findViewById(R.id.setting_menu).setVisibility(View.VISIBLE);
+                        findViewById(R.id.fab).setVisibility(View.VISIBLE);
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,RoutineFragment).commit();
                         return true;
 
                     case R.id.third_tab:
                         findViewById(R.id.setting_menu).setVisibility(View.VISIBLE);
+                        findViewById(R.id.fab).setVisibility(View.GONE);
                         getSupportFragmentManager().beginTransaction().replace(R.id.container,RecordFragment).commit();
                         return true;
 
@@ -116,5 +118,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void onBackPressed() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("종료하시겠습니까?");
+
+        alert.setPositiveButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        alert.setNegativeButton("종료", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        alert.show();
+    }
 }
+
+
 
