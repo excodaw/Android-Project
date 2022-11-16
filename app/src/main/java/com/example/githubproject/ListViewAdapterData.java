acadcbf6aea3ec7b3eb027275125e6ac449e932c
excodaw <dhtpgkr1999@gmail.com>
2022-11-16 20:49:40 +0900

    WorkOutFragment.java에 리스트 뷰 구현

--- app/src/main/java/com/example/githubproject/ListViewAdapterData.java
+++ app/src/main/java/com/example/githubproject/ListViewAdapterData.java
@@ -2,24 +2,11 @@ package com.example.githubproject;
 
 public class ListViewAdapterData {
     private String workout_name;
-    private String workout_type;
-    private int num;
     private int tts;
 
-    public ListViewAdapterData(int num, String workout_type, String workout_name, int tts) {
-        this.num = num;
-        this.workout_type = workout_type;
-        this.workout_name = workout_name;
-        this.tts = tts;
-    }
-
     public String getWorkout_name() {return this.workout_name;}
-    public String getWorkout_type() {return this.workout_type;}
-    public int getNum() {return this.num;}
     public int getTts() {return this.tts;}
 
     public void setWorkout_name(String workout_name) {this.workout_name = workout_name;}
-    public void setWorkout_type(String workout_type) {this.workout_type = workout_type;}
-    public void setNum(int num) {this.num = num;}
     public void setTts(int tts) {this.tts = tts;}
 }
