b39d4f87f1afa839b231c85612ffc7618033f974
pangyokyk <sh09202@naver.com>
2022-11-17 10:58:04 +0900

    오늘의 결과, 구현 숫자, 텍스트, 날짜 입력시 records.db 에 저장됨 2번쨰

--- app/src/main/java/com/example/githubproject/record_listview_BodyFat.java
+++ app/src/main/java/com/example/githubproject/record_listview_BodyFat.java
@@ -1,14 +1,39 @@
 package com.example.githubproject;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.security.AccessController.getContext;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class record_listview_BodyFat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_record_listview_body_fat);
+        setContentView(R.layout.activity_record_listview_muscle);
     }
 }
