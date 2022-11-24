87d6aff368672b876eaad612be0a762429f7dc78
ahnjinwon <anjy0821@naver.com>
2022-11-24 14:20:28 +0900

    루틴DB추가

--- app/src/main/java/com/example/githubproject/Routine_DBHelper.java
+++ app/src/main/java/com/example/githubproject/Routine_DBHelper.java
@@ -16,29 +16,29 @@ public class Routine_DBHelper extends SQLiteOpenHelper{
         // Person Table 생성
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE Record(number INT,Record_Type TEXT,Record_Date TEXT)");
+            db.execSQL("CREATE TABLE Routine(Exercise_Name TEXT,Time INT,TTS INT)");
         }
 
         // Person Table Upgrade
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("DROP TABLE IF EXISTS Record");
+            db.execSQL("DROP TABLE IF EXISTS Routine");
             onCreate(db);
         }
 
         // Person Table 데이터 입력
-        public void insert(int number, String Record_type, String Record_date) {
+        public void insert(String Exercise_Name, int Time, int TTS) {
             SQLiteDatabase db = getWritableDatabase();
-            db.execSQL("INSERT INTO Record VALUES(" + number + ", '" + Record_type + "', '" + Record_date + "')");
+            db.execSQL("INSERT INTO Routine VALUES('" + Exercise_Name + "', " + Time + ", " + TTS + ")");
             db.close();
         }
 
-//    // Person Table 데이터 수정
-//    public void Update(int ID, String Exercise_type, String Exercise_Name, int is_tts) {
-//        SQLiteDatabase db = getWritableDatabase();
-//        db.execSQL("UPDATE Exercise SET ID = " + ID + "', " + Exercise_type + ", '" + Exercise_Name + ", '" + is_tts + "'" + " WHERE ID = '" + ID + "'");
-//        db.close();
-//    }
+    // Person Table 데이터 수정
+    public void Update( int Time ) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("UPDATE Routine SET Time = Time WHERE Time = '" + Time + "'");
+        db.close();
+    }
 
 //    // Person Table 데이터 삭제
 //    public void Delete(String ID) {
@@ -48,22 +48,22 @@ public class Routine_DBHelper extends SQLiteOpenHelper{
 //    }
 
         // Person Table 조회
-        public String getResult() {
-            // 읽기가 가능하게 DB 열기
-            SQLiteDatabase db = getReadableDatabase();
-            String result = "";
-
-            // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
-            Cursor cursor = db.rawQuery("SELECT * FROM Record", null);
-            while (cursor.moveToNext()) {
-                result += " 수치 : " + cursor.getInt(0)
-                        + ", 측정목록 : "
-                        + cursor.getString(1)
-                        + ", 날짜 : "
-                        + cursor.getString(2)
-                        + "\n";
-            }
-
-            return result;
-        }
+//        public String getResult() {
+//            // 읽기가 가능하게 DB 열기
+//            SQLiteDatabase db = getReadableDatabase();
+//            String result = "";
+//
+//            // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
+//            Cursor cursor = db.rawQuery("SELECT * FROM Routine", null);
+//            while (cursor.moveToNext()) {
+//                result += " 수치 : " + cursor.getInt(0)
+//                        + ", 측정목록 : "
+//                        + cursor.getString(1)
+//                        + ", 날짜 : "
+//                        + cursor.getString(2)
+//                        + "\n";
+//            }
+//
+//            return result;
+//        }
 }
