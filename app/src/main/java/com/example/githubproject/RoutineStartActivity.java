c7e6c74c93fd79d74d849fa008b491d42996b4e6
ahnjinwon <anjy0821@naver.com>
2022-11-25 13:54:16 +0900

    타이머기능 추가

--- app/src/main/java/com/example/githubproject/RoutineStartActivity.java
+++ app/src/main/java/com/example/githubproject/RoutineStartActivity.java
@@ -1,16 +1,104 @@
 package com.example.githubproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class RoutineStartActivity extends AppCompatActivity{
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_routine_start);
+    LinearLayout timeCountSettingLV, timeCountLV;
+    TextView hourTV, minuteTV, secondTV, finishTV;
+    Button startBtn, endBtn;
+    int hour, minute, second;
 
 
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine_start);
+
+        //timeCountSettingLV = (LinearLayout)findViewById(R.id.timeCountSettingLV);
+        timeCountLV = (LinearLayout)findViewById(R.id.timeCountLV);
+
+        hourTV = (TextView)findViewById(R.id.hourTV);
+        minuteTV = (TextView)findViewById(R.id.minuteTV);
+        secondTV = (TextView)findViewById(R.id.secondTV);
+        finishTV = (TextView)findViewById(R.id.finishTV);
+
+        startBtn = (Button)findViewById(R.id.startBtn);
+        endBtn= (Button) findViewById((R.id.endBtn));
+
+        // 시작버튼 이벤트 1처리
+
+                //timeCountSettingLV.setVisibility(View.GONE);
+                timeCountLV.setVisibility(View.VISIBLE);
+
+                hourTV.setText("00");
+                minuteTV.setText("00");
+                secondTV.setText("00");
+
+                hour = 00;
+                minute = 00;
+                second = 00;
+
+                Timer timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        // 반복실행할 구문
+
+                        if(second >= 0) {
+                            //1초씩 증가
+                            second++;
+                        }
+
+                        //시, 분, 초가 10이하(한자리수) 라면
+                        // 숫자 앞에 0을 붙인다 ( 8 -> 08 )
+                        if(second <= 9){
+                            secondTV.setText("0" + second);
+                        } else {
+                            secondTV.setText(Integer.toString(second));
+                        }
+
+                        if(minute <= 9){
+                            minuteTV.setText("0" + minute);
+                        } else {
+                            minuteTV.setText(Integer.toString(minute));
+                        }
+
+                        if(hour <= 9){
+                            hourTV.setText("0" + hour);
+                        } else {
+                            hourTV.setText(Integer.toString(hour));
+                        }
+
+                        // 시분초가 다 0이라면 toast를 띄우고 타이머를 종료한다..
+                        if(hour == 0 && minute == 0 && second == 0) {
+                            timer.cancel();//타이머 종료
+                            finishTV.setText("타이머가 종료되었습니다.");
+                        }
+                    }
+                };
+
+                //타이머를 실행
+                timer.schedule(timerTask, 0, 1000); //Timer 실행
+
+        endBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
