5d5f06763cd3b5611ebf95224ad051431cd24fc6
ahnjinwon <anjy0821@naver.com>
2022-11-29 15:10:17 +0900

    루틴 데이터베이스 루틴명 추가, 루틴 생성 시 루틴 이름 설정

--- app/src/main/java/com/example/githubproject/AddRoutineActivity.java
+++ app/src/main/java/com/example/githubproject/AddRoutineActivity.java
@@ -1,19 +1,27 @@
 package com.example.githubproject;
 
+import static java.security.AccessController.getContext;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,6 +29,7 @@ import java.util.ArrayList;
 public class AddRoutineActivity extends AppCompatActivity {
     ListView workoutlistview;
     ListViewItemAdapter item = new ListViewItemAdapter();
+    String r_name;
 
 
     public boolean onCreateOptionsMenu(Menu menu)    {
@@ -31,12 +40,22 @@ public int count=0;
     public boolean onOptionsItemSelected(MenuItem items) {
         switch(items.getItemId()) {
             case R.id.add_menu:
+                final EditText et = new EditText(this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("루틴 생성").setMessage("루틴 이름을 입력하세요").setView(et).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        r_name = et.getText().toString();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
                 for (int i=0; i<item.getCount(); i++){
                     if (item.isChecked(i)== false) {
                         count++;
                         }
 
                     else if (item.isChecked(i)==true){
+
                         Log.v("tag","name="+item.getItem(i));
                         DBHelper helper = new DBHelper(this, 1);
                         SQLiteDatabase db = helper.getReadableDatabase();
@@ -48,11 +67,10 @@ public int count=0;
                                 Routine_DBHelper r_helper = new Routine_DBHelper(this,1);
                                 SQLiteDatabase r_db = r_helper.getWritableDatabase();
 
-                                r_helper.insert(item.getItem(i).toString(),0, cursor.getInt(3));
+                                r_helper.insert(r_name,item.getItem(i).toString(),0, cursor.getInt(3));
                                 r_db.close();
                             }
                         }
-
                         db.close();
 //                        Routine_DBHelper r_helper = new Routine_DBHelper(this,1);
 //                                SQLiteDatabase r_db = r_helper.getWritableDatabase();
