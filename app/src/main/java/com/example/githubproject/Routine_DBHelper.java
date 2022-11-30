b1db9f8b25a458cd8c26c8d13b78a6e67ae8dde1
excodaw <dhtpgkr1999@gmail.com>
2022-11-30 17:51:17 +0900

    루틴 목록 리스트 업
    루틴 삭제 기능 추가

--- app/src/main/java/com/example/githubproject/Routine_DBHelper.java
+++ app/src/main/java/com/example/githubproject/Routine_DBHelper.java
@@ -16,7 +16,7 @@ public class Routine_DBHelper extends SQLiteOpenHelper{
         // Person Table 생성
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL("CREATE TABLE Routine(Routine_Name TEXT,Exercise_Name TEXT,Time INT,TTS INT)");
+            db.execSQL("CREATE TABLE Routine(Routine_Name TEXT, Exercise_Name TEXT, Time INT, TTS INT)");
         }
 
         // Person Table Upgrade
@@ -29,7 +29,7 @@ public class Routine_DBHelper extends SQLiteOpenHelper{
         // Person Table 데이터 입력
         public void insert(String Routine_Name, String Exercise_Name, int Time, int TTS) {
             SQLiteDatabase db = getWritableDatabase();
-            db.execSQL("INSERT INTO Routine VALUES('"+ Routine_Name + "', '" + Exercise_Name +"',"+ Time + ", "+ TTS + ")");
+            db.execSQL("INSERT INTO Routine VALUES("+ Routine_Name + ", '" + Exercise_Name +"','"+ Time + "', "+ TTS + ")");
             db.close();
         }
 
@@ -40,30 +40,10 @@ public class Routine_DBHelper extends SQLiteOpenHelper{
         db.close();
     }
 
-//    // Person Table 데이터 삭제
-//    public void Delete(String ID) {
-//        SQLiteDatabase db = getWritableDatabase();
-//        db.execSQL("DELETE Exercise WHERE ID = '" + ID + "'");
-//        db.close();
-//    }
+    public void delete(String routine_name) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DELETE FROM Routine WHERE Routine_Name = '" + routine_name + "'");
+        db.close();
+    }
 
-        // Person Table 조회
-//        public String getResult() {
-//            // 읽기가 가능하게 DB 열기
-//            SQLiteDatabase db = getReadableDatabase();
-//            String result = "";
-//
-//            // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
-//            Cursor cursor = db.rawQuery("SELECT * FROM Routine", null);
-//            while (cursor.moveToNext()) {
-//                result += " 수치 : " + cursor.getInt(0)
-//                        + ", 측정목록 : "
-//                        + cursor.getString(1)
-//                        + ", 날짜 : "
-//                        + cursor.getString(2)
-//                        + "\n";
-//            }
-//
-//            return result;
-//        }
 }
