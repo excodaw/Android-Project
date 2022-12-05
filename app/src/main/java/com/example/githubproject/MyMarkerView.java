f5ccb73ebda4168e861b40a637486f5a14ffd66c
pangyokyk <sh09202@naver.com>
2022-12-05 21:39:48 +0900

    마커구현 그래프수정

---
+++ app/src/main/java/com/example/githubproject/MyMarkerView.java
@@ -0,0 +1,50 @@
+package com.example.githubproject;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyMarkerView extends MarkerView {
+
+    private TextView tvContent;
+
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        tvContent = (TextView)findViewById(R.id.tvContent);
+
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+
+    public void refreshContent(Entry e, Highlight highlight) {
+
+        if (e instanceof CandleEntry) {
+
+            CandleEntry ce = (CandleEntry) e;
+
+            tvContent.setText("" + Utils.formatNumber(ce.getHigh(), 0, true)+"" + Utils.formatNumber(ce.getHigh(), 0, true));
+        } else {
+
+            tvContent.setText("" + Utils.formatNumber(e.getX()+1, 0, true)+"번째 측정\n" + Utils.formatNumber(e.getY(), 0, true)+"kg");
+        }
+
+        super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF(-(getWidth() / 2), -getHeight());
+    }
+}
